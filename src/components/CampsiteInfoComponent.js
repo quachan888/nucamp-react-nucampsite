9fd9fd862a313a23d03702084109046200820b9d
An Quach <quachan888@gmail.com>
2022-01-22 12:58:38 -0500

    finish Week 2 assignment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,8 +26,8 @@ class CampsiteInfo extends Component {
             return (
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {this.props.campsite.comments.map((comment) => (
-                        <p>
+                    {this.props.campsite.comments.map((comment, index) => (
+                        <p key={index}>
                             {comment.text}
                             <br />
                             -- {comment.author}, {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" }).format(new Date(Date.parse(comment.date)))}
