7771c9fb15dd2a4592a2a0a34aec44ef11bae196
An Quach <quachan888@gmail.com>
2022-02-07 12:55:05 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -35,7 +35,7 @@ function RenderCampsite({ campsite }) {
     return <div></div>;
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -52,7 +52,7 @@ function RenderComments({ comments }) {
                         }).format(new Date(Date.parse(comment.date)))}
                     </p>
                 ))}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -72,9 +72,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit = (values) => {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     };
 
     toggleModal = () => {
@@ -175,7 +174,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,14 +10,19 @@ import Header from './Headercomponents';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import { addComment } from '../redux/ActionCreators';
 
 const mapStateToProps = (state) => {
     return {
         campsites: state.campsites,
         comments: state.comments,
         partners: state.partners,
-        promotions: state.promotions,
+        promotions: state.promotions
     };
+};
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => addComment(campsiteId, rating, author, text)
 };
 
 class Main extends Component {
@@ -37,6 +42,7 @@ class Main extends Component {
                 <CampsiteInfo
                     campsite={this.props.campsites.filter((campsite) => campsite.id === +match.params.campsiteId)[0]}
                     comments={this.props.comments.filter((comment) => comment.campsiteId === +match.params.campsiteId)}
+                    addComment={this.props.addComment}
                 />
             );
         };
@@ -58,4 +64,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
