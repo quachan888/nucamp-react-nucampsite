16b8a02f71d47565920ac3dd501109742b2e1d2e
An Quach <quachan888@gmail.com>
2022-02-07 12:42:31 -0500

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Comments } from './comments';
+import { Campsites } from './campsites';
+import { Promotions } from './promotions';
+import { Partners } from './partners';
 
 export const ConfigureStore = () => {
-    const store = createStore(Reducer, initialState);
+    const store = createStore(
+        combineReducers({
+            campsites: Campsites,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions
+        })
+    );
     return store;
 };
